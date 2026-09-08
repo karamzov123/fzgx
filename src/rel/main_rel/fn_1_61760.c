3cf38210729e5703c8f44da675fd2de09a867efb
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:26:40 -0400

    match: main_rel/fn_1_61760 (Matched fn_1_61760 at 100%.)

--- src/rel/main_rel/fn_1_61760.c
+++ src/rel/main_rel/fn_1_61760.c
@@ -1,4 +1,16 @@
 #include "types.h"
 
-// fn_1_61760: main_rel .text:0x00061760 size 0x44
-// Carved by fzgx. Replace this file's body with the matching C.
+extern u8 lbl_1_data_1D62C[148];
+extern void *lbl_801A6410;
+extern void fn_1_46B4(void *, void *, u8 *, s32);
+
+struct Fn1_61760Object {
+    u8 _pad38[0x38];
+    void *field_38;
+};
+
+void fn_1_61760(struct Fn1_61760Object *object) {
+    if (object->field_38 != 0) {
+        fn_1_46B4(lbl_801A6410, object->field_38, lbl_1_data_1D62C, 0x16c1);
+    }
+}
