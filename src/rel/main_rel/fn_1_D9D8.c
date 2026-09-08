6c37cac803b3ea37dc3d9255fc8d417f765f3ca4
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:01:40 -0400

    match: main_rel/fn_1_D9D8 (fn_1_D9D8: leaf function returns address of BSS array. Simple lis/addi load + blr return pattern.)

---
+++ src/rel/main_rel/fn_1_D9D8.c
@@ -0,0 +1,10 @@
+#include "types.h"
+
+// fn_1_D9D8: main_rel .text:0x0000D9D8 size 0xC
+// Returns address of lbl_1_bss_108C
+
+extern u8 lbl_1_bss_108C[52];
+
+u8* fn_1_D9D8(void) {
+    return lbl_1_bss_108C;
+}
