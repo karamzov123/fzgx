5e95fb29122ed8f905d1c701c86e3a632179a40b
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:25:43 -0400

    match: main_rel/fn_1_E5724 (Match signed halfword load at offset 0x8.)

--- src/rel/main_rel/fn_1_E5724.c
+++ src/rel/main_rel/fn_1_E5724.c
@@ -1,4 +1,5 @@
 #include "types.h"
 
-// fn_1_E5724: main_rel .text:0x000E5724 size 0x8
-// Carved by fzgx. Replace this file's body with the matching C.
+s16 fn_1_E5724(void *base) {
+    return *(s16 *)((char *)base + 0x8);
+}
