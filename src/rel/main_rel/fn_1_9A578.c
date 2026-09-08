615191b2c5c9bf2167bf2031ab2b4e1a5d147045
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:31:45 -0400

    match: main_rel/fn_1_9A578 (Match fn_1_9A578 with callback table dispatch.)

--- src/rel/main_rel/fn_1_9A578.c
+++ src/rel/main_rel/fn_1_9A578.c
@@ -1,3 +1,21 @@
 #include "types.h"
 
-// fn_1_9A578: carved by fzgx; best attempt saved to fn_1_9A578.1788837528.c
+extern s16 lbl_1_data_2A7E0;
+extern void fn_1_10302C(void);
+
+typedef void (*fn_1_9A578_callback)(void);
+
+struct fn_1_9A578_entry {
+    u8 pad[8];
+    fn_1_9A578_callback callback;
+    u8 rest[0x18];
+};
+
+extern struct fn_1_9A578_entry lbl_1_data_2ABAC[];
+
+void fn_1_9A578(void) {
+    if (lbl_1_data_2A7E0 > 0) {
+        fn_1_10302C();
+        lbl_1_data_2ABAC[lbl_1_data_2A7E0].callback();
+    }
+}
