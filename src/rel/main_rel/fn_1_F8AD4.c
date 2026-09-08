e704ee6758d4d69cc8d6b8f0fc5acb81f1f66cfc
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:37:13 -0400

    match: main_rel/fn_1_F8AD4 (match fn_1_F8AD4)

--- src/rel/main_rel/fn_1_F8AD4.c
+++ src/rel/main_rel/fn_1_F8AD4.c
@@ -1,4 +1,7 @@
 #include "types.h"
 
-// fn_1_F8AD4: main_rel .text:0x000F8AD4 size 0x28
-// Carved by fzgx. Replace this file's body with the matching C.
+extern u32 lbl_1_bss_7F0C0[5336];
+
+void fn_1_F8AD4(u32 value) {
+    lbl_1_bss_7F0C0[4672] |= (0x8000u << 16) >> ((value & 0xff) + 0x10);
+}
