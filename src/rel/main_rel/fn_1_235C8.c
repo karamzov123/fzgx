e011c44431edf28f087e3a2b279684738402968d
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:38:23 -0400

    match: main_rel/fn_1_235C8 (match fn_1_235C8)

--- src/rel/main_rel/fn_1_235C8.c
+++ src/rel/main_rel/fn_1_235C8.c
@@ -1,4 +1,18 @@
 #include "types.h"
 
-// fn_1_235C8: main_rel .text:0x000235C8 size 0x48
-// Carved by fzgx. Replace this file's body with the matching C.
+extern void *memset(void *, int, unsigned int);
+
+typedef struct Fn_1_235C8 {
+    unsigned char *field00;
+    unsigned char *field04;
+    unsigned char data[0xb8];
+} Fn_1_235C8;
+
+void fn_1_235C8(Fn_1_235C8 *self) {
+    if (self != 0) {
+        memset(self, 0, 0xc0);
+        self->field00 = &self->data[0];
+        self->field04 = &self->data[0x5c];
+    }
+}
+
