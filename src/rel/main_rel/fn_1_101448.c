b24bcaf2d11ccc364e8049b18def3636ec0a3199
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 22:41:42 -0400

    match: main_rel/fn_1_101448 (Matched empty function (single blr instruction))

--- src/rel/main_rel/fn_1_101448.c
+++ src/rel/main_rel/fn_1_101448.c
@@ -1,4 +1,5 @@
 #include "types.h"
 
-// fn_1_101448: main_rel .text:0x00101448 size 0x4
-// Carved by fzgx. Replace this file's body with the matching C.
+// fn_1_101448: Empty function (blr only)
+void fn_1_101448(void) {
+}
