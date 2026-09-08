fdb0f61a6a2ed91d9a88dc6896a3e35e9885e7d6
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:38:37 -0400

    match: main_rel/fn_1_15B588 (Matched fn_1_15B588 at 100.0% under GC/1.3.2.)

--- src/rel/main_rel/fn_1_15B588.c
+++ src/rel/main_rel/fn_1_15B588.c
@@ -1,4 +1,19 @@
 #include "types.h"
 
-// fn_1_15B588: main_rel .text:0x0015B588 size 0x44
-// Carved by fzgx. Replace this file's body with the matching C.
+extern u32 lbl_1_data_2A7E0[62];
+
+void fn_1_15B588(void) {
+    u8 *entry;
+    s16 i;
+    u8 *obj;
+
+    obj = (u8 *)lbl_1_data_2A7E0[15];
+    entry = obj;
+    i = 0;
+    while (i < *(u8 *)(obj + 0x5e)) {
+        u32 *value = *(u32 **)(entry + 0xc4);
+        entry += 4;
+        i++;
+        *value &= 0x7fffffff;
+    }
+}
