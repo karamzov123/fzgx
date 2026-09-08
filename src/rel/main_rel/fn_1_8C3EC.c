f57fdda7499833802c8f289fc0d9d3e3c86e8930
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:35:15 -0400

    match: main_rel/fn_1_8C3EC (Match fn_1_8C3EC as a direct return of lbl_1_bss_6DA7A.)

--- src/rel/main_rel/fn_1_8C3EC.c
+++ src/rel/main_rel/fn_1_8C3EC.c
@@ -1,4 +1,7 @@
 #include "types.h"
 
-// fn_1_8C3EC: main_rel .text:0x0008C3EC size 0x10
-// Carved by fzgx. Replace this file's body with the matching C.
+extern u8 lbl_1_bss_6DA7A;
+
+u8 fn_1_8C3EC(void) {
+    return lbl_1_bss_6DA7A;
+}
