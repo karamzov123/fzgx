eb645039ce4f52ee87036b278587f8d3a2e8b90f
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:38:05 -0400

    match: main_rel/fn_1_4100C (Matched two initialization calls with distinct data objects.)

--- src/rel/main_rel/fn_1_4100C.c
+++ src/rel/main_rel/fn_1_4100C.c
@@ -1,4 +1,10 @@
 #include "types.h"
 
-// fn_1_4100C: main_rel .text:0x0004100C size 0x3C
-// Carved by fzgx. Replace this file's body with the matching C.
+extern char lbl_1_data_66E0[16];
+extern char lbl_1_data_66F0[16];
+extern void fn_1_465D0(char *arg0, u32 arg1);
+
+void fn_1_4100C(void) {
+    fn_1_465D0(lbl_1_data_66E0, 1);
+    fn_1_465D0(lbl_1_data_66F0, 1);
+}
