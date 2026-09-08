1eb209e58d743a130b2d22cc0c1fed53e812c6eb
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:25:48 -0400

    match: main_rel/fn_1_4DDA0 (Matched fn_1_4DDA0 at 100%.)

--- src/rel/main_rel/fn_1_4DDA0.c
+++ src/rel/main_rel/fn_1_4DDA0.c
@@ -1,4 +1,7 @@
 #include "types.h"
 
-// fn_1_4DDA0: main_rel .text:0x0004DDA0 size 0x10
-// Carved by fzgx. Replace this file's body with the matching C.
+extern u32 lbl_1_bss_4C688[2048];
+
+u8 *fn_1_4DDA0(void) {
+    return (u8 *)lbl_1_bss_4C688 + 6;
+}
