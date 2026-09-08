b49da7dbfcd5a08ab9cf483ed2311b5dd2a462f1
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:35:18 -0400

    match: main_rel/fn_1_FB180 (matched store of 1 through pointer)

--- src/rel/main_rel/fn_1_FB180.c
+++ src/rel/main_rel/fn_1_FB180.c
@@ -1,4 +1,5 @@
 #include "types.h"
 
-// fn_1_FB180: main_rel .text:0x000FB180 size 0xC
-// Carved by fzgx. Replace this file's body with the matching C.
+void fn_1_FB180(int *value) {
+    *value = 1;
+}
