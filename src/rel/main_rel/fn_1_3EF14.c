9c8531f5744ff2c0fb9eec993a5632a1fb4280ca
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:40:45 -0400

    match: main_rel/fn_1_3EF14 (Matched fn_1_3EF14 at 100% under GC/1.3.2.)

--- src/rel/main_rel/fn_1_3EF14.c
+++ src/rel/main_rel/fn_1_3EF14.c
@@ -1,4 +1,19 @@
 #include "types.h"
 
-// fn_1_3EF14: main_rel .text:0x0003EF14 size 0x78
-// Carved by fzgx. Replace this file's body with the matching C.
+extern u8 lbl_1_bss_3C12[10];
+extern s16 lbl_1_bss_960;
+extern u8 lbl_1_bss_3C30[5308];
+
+extern void fn_80008BA8(void *arg1, u8 *arg2, u32 arg3);
+extern void fn_80008BEC(void *arg1, u32 arg2, u32 arg3);
+
+void fn_1_3EF14(void *arg1) {
+    if (lbl_1_bss_3C12[0] != 0 ||
+        ((u16)(lbl_1_bss_960 - 13) <= 1) ||
+        lbl_1_bss_960 == 10 ||
+        lbl_1_bss_960 == 16) {
+        fn_80008BA8(arg1, lbl_1_bss_3C30, 0x14b8);
+    } else {
+        fn_80008BEC(arg1, 0, 0x14b8);
+    }
+}
