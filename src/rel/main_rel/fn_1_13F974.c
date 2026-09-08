d341d28b11e58dfd238c3eec541caaa5d5419633
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:39:54 -0400

    match: main_rel/fn_1_13F974 (Matched fn_1_13F974 at 100% under GC/1.3.2.)

--- src/rel/main_rel/fn_1_13F974.c
+++ src/rel/main_rel/fn_1_13F974.c
@@ -1,4 +1,16 @@
 #include "types.h"
 
-// fn_1_13F974: main_rel .text:0x0013F974 size 0x68
-// Carved by fzgx. Replace this file's body with the matching C.
+extern u32 lbl_801A6410;
+extern char lbl_1_data_419E0[18];
+extern u32 fn_1_45D0(u32 arg0, int arg1, void* arg2, int arg3);
+extern u32 lbl_1_bss_8E404[8];
+extern u32 fn_80008BEC(u32 arg0, int arg1, int arg2);
+
+u32 fn_1_13F974(u32 arg0) {
+    u32* config = &lbl_801A6410;
+
+    lbl_1_bss_8E404[arg0 & 0xFF] =
+        fn_1_45D0(config[0], 0x440, lbl_1_data_419E0, 0x1528);
+    return fn_80008BEC(lbl_1_bss_8E404[arg0 & 0xFF], 0, 0x440);
+}
+
