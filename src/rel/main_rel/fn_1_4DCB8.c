278d2aa5d4d0cab5d894d8fa9d79016c8cf9968a
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:36:13 -0400

    match: main_rel/fn_1_4DCB8 (Matched global swap accessor at lbl_1_bss_4B9CC+0x58.)

--- src/rel/main_rel/fn_1_4DCB8.c
+++ src/rel/main_rel/fn_1_4DCB8.c
@@ -1,4 +1,10 @@
 #include "types.h"
 
-// fn_1_4DCB8: main_rel .text:0x0004DCB8 size 0x18
-// Carved by fzgx. Replace this file's body with the matching C.
+extern u32 lbl_1_bss_4B9CC[23];
+
+u32 fn_1_4DCB8(u32 value) {
+    u32 old_value = lbl_1_bss_4B9CC[22];
+    lbl_1_bss_4B9CC[22] = value;
+    return old_value;
+}
+
