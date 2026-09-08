79b50e84262db9eb40c06e22f403b98d121ae27e
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:36:08 -0400

    match: main_rel/fn_1_FA070 (match fn_1_FA070)

--- src/rel/main_rel/fn_1_FA070.c
+++ src/rel/main_rel/fn_1_FA070.c
@@ -1,4 +1,8 @@
 #include "types.h"
 
-// fn_1_FA070: main_rel .text:0x000FA070 size 0x18
-// Carved by fzgx. Replace this file's body with the matching C.
+extern u32 lbl_1_bss_84420[2];
+
+u32 fn_1_FA070(void) {
+    u32 ptr = lbl_1_bss_84420[0];
+    return (*(u8 *)(ptr + 0x2)) & 1;
+}
