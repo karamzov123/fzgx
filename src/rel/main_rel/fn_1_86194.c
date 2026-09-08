e5c6508861773df0cfabad3c0bf3ee137e7988d2
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 22:41:34 -0400

    match: main_rel/fn_1_86194 (Matched fn_1_86194: simple wrapper calling fn_1_85934.)

--- src/rel/main_rel/fn_1_86194.c
+++ src/rel/main_rel/fn_1_86194.c
@@ -1,4 +1,10 @@
 #include "types.h"
 
+extern void fn_1_85934(void);
+
 // fn_1_86194: main_rel .text:0x00086194 size 0x20
-// Carved by fzgx. Replace this file's body with the matching C.
+// Wrapper that calls fn_1_85934.
+
+void fn_1_86194(void) {
+    fn_1_85934();
+}
