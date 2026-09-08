93012ffdba5a8e0f18b55e11f595e54e3fadb9da
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:35:12 -0400

    match: main_rel/fn_1_D0E34 (Match pointer return to lbl_1_data_3D648 + 0x20.)

--- src/rel/main_rel/fn_1_D0E34.c
+++ src/rel/main_rel/fn_1_D0E34.c
@@ -1,4 +1,7 @@
 #include "types.h"
 
-// fn_1_D0E34: main_rel .text:0x000D0E34 size 0x10
-// Carved by fzgx. Replace this file's body with the matching C.
+extern u16 lbl_1_data_3D648[20];
+
+u8* fn_1_D0E34(void) {
+    return (u8*)lbl_1_data_3D648 + 0x20;
+}
