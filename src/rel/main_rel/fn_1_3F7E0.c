65e7b64c1529b8eb88c9c1e5e725c4854e3530db
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:36:27 -0400

    match: main_rel/fn_1_3F7E0 (Matched fn_1_3F7E0 at 100%.)

--- src/rel/main_rel/fn_1_3F7E0.c
+++ src/rel/main_rel/fn_1_3F7E0.c
@@ -1,4 +1,11 @@
 #include "types.h"
 
-// fn_1_3F7E0: main_rel .text:0x0003F7E0 size 0x20
-// Carved by fzgx. Replace this file's body with the matching C.
+extern u8 lbl_1_bss_3C30[5308];
+
+u8 fn_1_3F7E0(void) {
+    u32 value = *(u32*)lbl_1_bss_3C30;
+    if (value & 0x00008000) {
+        return lbl_1_bss_3C30[9];
+    }
+    return lbl_1_bss_3C30[10];
+}
