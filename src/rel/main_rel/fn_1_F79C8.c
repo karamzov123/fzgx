c4a4dd3f9538fc80cf7abd331cb0db60ea795b0a
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:26:56 -0400

    match: main_rel/fn_1_F79C8 (Matched fn_1_F79C8 at 100%.)

--- src/rel/main_rel/fn_1_F79C8.c
+++ src/rel/main_rel/fn_1_F79C8.c
@@ -1,4 +1,13 @@
 #include "types.h"
 
-// fn_1_F79C8: main_rel .text:0x000F79C8 size 0x58
-// Carved by fzgx. Replace this file's body with the matching C.
+extern u8 lbl_1_bss_718E0[23016];
+extern u32 lbl_1_bss_7F0C0[5336];
+extern void fn_1_F8918(u8 *, u32 *, u8 *);
+extern u32 lbl_1_bss_84420[2];
+extern u16 lbl_1_data_414[36];
+
+void fn_1_F79C8(void) {
+    fn_1_F8918(&lbl_1_bss_718E0[0x1c0], lbl_1_bss_7F0C0, lbl_1_bss_718E0);
+    lbl_1_bss_84420[0] = (u32)((u8 *)lbl_1_bss_7F0C0 + 0x4b5c);
+    lbl_1_data_414[0] = 0xffff;
+}
