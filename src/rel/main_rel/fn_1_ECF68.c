c1b2c81e1e46b736967688861225f4600d69ad35
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:36:29 -0400

    match: main_rel/fn_1_ECF68 (Matched wrapper calling fn_1_F23E8.)

--- src/rel/main_rel/fn_1_ECF68.c
+++ src/rel/main_rel/fn_1_ECF68.c
@@ -1,4 +1,7 @@
 #include "types.h"
 
-// fn_1_ECF68: main_rel .text:0x000ECF68 size 0x20
-// Carved by fzgx. Replace this file's body with the matching C.
+extern void fn_1_F23E8(void);
+
+void fn_1_ECF68(void) {
+    fn_1_F23E8();
+}
