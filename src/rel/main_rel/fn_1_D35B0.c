74f1ba6ae377b65430463b32789e618e4db6b230
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 22:41:41 -0400

    match: main_rel/fn_1_D35B0 (Matched fn_1_D35B0: simple wrapper calling fn_80071100 followed by fn_1_D3B6C.)

--- src/rel/main_rel/fn_1_D35B0.c
+++ src/rel/main_rel/fn_1_D35B0.c
@@ -1,4 +1,10 @@
 #include "types.h"
 
-// fn_1_D35B0: main_rel .text:0x000D35B0 size 0x24
-// Carved by fzgx. Replace this file's body with the matching C.
+extern void fn_80071100(void);
+extern void fn_1_D3B6C(void);
+
+// Wrapper that calls fn_80071100 followed by fn_1_D3B6C.
+void fn_1_D35B0(void) {
+    fn_80071100();
+    fn_1_D3B6C();
+}
