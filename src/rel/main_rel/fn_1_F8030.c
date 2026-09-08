62ee9831365dd9411291c9574dbf71cd8b050b5c
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:25:58 -0400

    match: main_rel/fn_1_F8030 (Matched fn_1_F8030 with indexed stores to the two global fields.)

--- src/rel/main_rel/fn_1_F8030.c
+++ src/rel/main_rel/fn_1_F8030.c
@@ -1,4 +1,8 @@
 #include "types.h"
 
-// fn_1_F8030: main_rel .text:0x000F8030 size 0x18
-// Carved by fzgx. Replace this file's body with the matching C.
+extern u32 lbl_1_bss_7F0C0[5336];
+
+void fn_1_F8030(void) {
+    lbl_1_bss_7F0C0[19] = 0;
+    lbl_1_bss_7F0C0[18] = 0;
+}
