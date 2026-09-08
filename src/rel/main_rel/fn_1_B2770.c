300df67b908db7b9311deb57a77fb32aecf4505a
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 22:37:44 -0400

    match: main_rel/fn_1_B2770 (Match fn_1_B2770: store byte value to offset in pointer)

--- src/rel/main_rel/fn_1_B2770.c
+++ src/rel/main_rel/fn_1_B2770.c
@@ -1,4 +1,8 @@
 #include "types.h"
 
 // fn_1_B2770: main_rel .text:0x000B2770 size 0xC
-// Carved by fzgx. Replace this file's body with the matching C.
+// Store byte 0x3 at offset 3 of the pointer argument
+
+void fn_1_B2770(int unused, void *ptr) {
+    *(u8 *)((u8 *)ptr + 3) = 0x3;
+}
