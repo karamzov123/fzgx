60744de0ed5dd32f5709d38e44b9fba88a91a279
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:27:02 -0400

    match: main_rel/fn_1_509C (Match fn_1_509C at 100%.)

--- src/rel/main_rel/fn_1_509C.c
+++ src/rel/main_rel/fn_1_509C.c
@@ -1,4 +1,16 @@
 #include "types.h"
 
-// fn_1_509C: main_rel .text:0x0000509C size 0x68
-// Carved by fzgx. Replace this file's body with the matching C.
+extern u32 lbl_1_bss_F58;
+extern u32 lbl_1_bss_7AC40[2];
+extern u32 lbl_1_bss_7AC20[8];
+
+s32 fn_8008023C(u32 *, u32 *, u32);
+
+s32 fn_1_509C(void) {
+    lbl_1_bss_7AC40[0] = lbl_1_bss_F58;
+    if (fn_8008023C(lbl_1_bss_7AC20, lbl_1_bss_7AC40, 4) == 0) {
+        return 0;
+    }
+    lbl_1_bss_7AC20[0] = lbl_1_bss_7AC40[0];
+    return 1;
+}
