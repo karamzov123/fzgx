6728821aede139308ec063749d939d60634ac49e
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:37:04 -0400

    match: main_rel/fn_1_103AA8 (Matched fn_1_103AA8 at 100%.)

--- src/rel/main_rel/fn_1_103AA8.c
+++ src/rel/main_rel/fn_1_103AA8.c
@@ -1,4 +1,11 @@
 #include "types.h"
 
-// fn_1_103AA8: main_rel .text:0x00103AA8 size 0x2C
-// Carved by fzgx. Replace this file's body with the matching C.
+extern f32 lbl_1_rodata_78E8[19];
+extern void fn_1_103B00(void);
+extern f32 lbl_1_bss_85290;
+extern u32 lbl_1_data_2A7E0[62];
+
+void fn_1_103AA8(void) {
+    lbl_1_bss_85290 = lbl_1_rodata_78E8[0];
+    lbl_1_data_2A7E0[13] = (u32)fn_1_103B00;
+}
