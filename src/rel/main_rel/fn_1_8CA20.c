f0d36b2d74a40247399e1c59fcd8cd1f19e299a4
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 22:44:38 -0400

    match: main_rel/fn_1_8CA20 (return lbl_1_bss_6D83C + 0x3a8; unit needed force_active (no callers, -strip_partial))

--- src/rel/main_rel/fn_1_8CA20.c
+++ src/rel/main_rel/fn_1_8CA20.c
@@ -1,3 +1,9 @@
 #include "types.h"
 
-// fn_1_8CA20: carved by fzgx; best attempt saved to fn_1_8CA20.1788835071.c
+// fn_1_8CA20: Load value from BSS and return with offset applied
+
+extern u32 lbl_1_bss_6D83C;
+
+u32 fn_1_8CA20(void) {
+    return lbl_1_bss_6D83C + 0x3a8;
+}
