925d627c3260c9754dc43fcce228a2f43a813b19
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:35:21 -0400

    match: main_rel/fn_1_D0DA8 (Matched pointer return to lbl_1_data_3D648 + 0x6.)

--- src/rel/main_rel/fn_1_D0DA8.c
+++ src/rel/main_rel/fn_1_D0DA8.c
@@ -1,4 +1,7 @@
 #include "types.h"
 
-// fn_1_D0DA8: main_rel .text:0x000D0DA8 size 0x10
-// Carved by fzgx. Replace this file's body with the matching C.
+extern u16 lbl_1_data_3D648[20];
+
+u8* fn_1_D0DA8(void) {
+    return (u8*)lbl_1_data_3D648 + 0x6;
+}
