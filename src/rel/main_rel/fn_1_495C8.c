4562b9d7c5a4fe55221087e349673e19580947dd
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:35:10 -0400

    match: main_rel/fn_1_495C8 (Matched byte store to BSS offset 0x36.)

--- src/rel/main_rel/fn_1_495C8.c
+++ src/rel/main_rel/fn_1_495C8.c
@@ -1,4 +1,7 @@
 #include "types.h"
 
-// fn_1_495C8: main_rel .text:0x000495C8 size 0x10
-// Carved by fzgx. Replace this file's body with the matching C.
+extern u32 lbl_1_bss_4B9CC[23];
+
+void fn_1_495C8(u8 value) {
+    *(u8 *)((u8 *)lbl_1_bss_4B9CC + 0x36) = value;
+}
