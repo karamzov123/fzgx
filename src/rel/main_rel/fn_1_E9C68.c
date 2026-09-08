dc23d6e9ae59f6667f55ac51397356cf2963b580
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:41:08 -0400

    match: main_rel/fn_1_E9C68 (match fn_1_E9C68)

--- src/rel/main_rel/fn_1_E9C68.c
+++ src/rel/main_rel/fn_1_E9C68.c
@@ -1,4 +1,30 @@
 #include "types.h"
 
-// fn_1_E9C68: main_rel .text:0x000E9C68 size 0x94
-// Carved by fzgx. Replace this file's body with the matching C.
+extern u32 lbl_1_bss_7B188[2];
+extern s16 lbl_1_bss_960;
+extern u32 fn_1_3F8C0(void);
+extern u8 lbl_1_bss_7C85C[113];
+extern u32 lbl_1_data_3E52C;
+extern u8 lbl_1_data_3E434[68];
+extern u32 fn_80008E84(u32 value);
+
+void fn_1_E9C68(void) {
+    u32 value;
+
+    if ((s32)lbl_1_bss_7B188[0] == 0) {
+        return;
+    }
+
+    if (lbl_1_bss_960 == 9) {
+        value = fn_80008E84(fn_1_3F8C0());
+        *(u32*)lbl_1_bss_7C85C = value;
+    }
+
+    if ((s32)lbl_1_data_3E52C >= 0) {
+        ((void (**)(void))lbl_1_data_3E434)[lbl_1_data_3E52C]();
+    }
+
+    if (lbl_1_bss_960 == 9) {
+        fn_80008E84(*(u32*)lbl_1_bss_7C85C);
+    }
+}
