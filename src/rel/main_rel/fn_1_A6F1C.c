8a3869a4982d75434a4e2e4d6021e8e0aad66107
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:26:12 -0400

    match: main_rel/fn_1_A6F1C (Matched global BSS load-return leaf function.)

--- src/rel/main_rel/fn_1_A6F1C.c
+++ src/rel/main_rel/fn_1_A6F1C.c
@@ -1,4 +1,7 @@
 #include "types.h"
 
-// fn_1_A6F1C: main_rel .text:0x000A6F1C size 0x10
-// Carved by fzgx. Replace this file's body with the matching C.
+extern u32 lbl_1_bss_6F5F0;
+
+u32 fn_1_A6F1C(void) {
+    return lbl_1_bss_6F5F0;
+}
