0aa7d3f13976880c0fefbf4c1a04a166cbaf23d2
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:38:55 -0400

    match: main_rel/fn_1_843BC (match fn_1_843BC)

--- src/rel/main_rel/fn_1_843BC.c
+++ src/rel/main_rel/fn_1_843BC.c
@@ -1,4 +1,21 @@
 #include "types.h"
 
-// fn_1_843BC: main_rel .text:0x000843BC size 0x4C
-// Carved by fzgx. Replace this file's body with the matching C.
+extern f32 lbl_1_rodata_3530[22];
+extern f32 fn_1_86A2C(void *);
+
+typedef struct {
+    u8 _pad[0x32c];
+    void *field_32c;
+} Fn1843BCObject;
+
+f32 fn_1_843BC(Fn1843BCObject *obj) {
+    if (obj == NULL) {
+        return lbl_1_rodata_3530[0];
+    }
+
+    if (obj->field_32c == NULL) {
+        return lbl_1_rodata_3530[0];
+    }
+
+    return fn_1_86A2C(obj->field_32c);
+}
