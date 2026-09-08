ebbb8d431187e2724609dee582cc2afac3ccf93d
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:35:39 -0400

    match: main_rel/fn_1_40ED4 (match fn_1_40ED4)

--- src/rel/main_rel/fn_1_40ED4.c
+++ src/rel/main_rel/fn_1_40ED4.c
@@ -1,4 +1,7 @@
 #include "types.h"
 
-// fn_1_40ED4: main_rel .text:0x00040ED4 size 0x10
-// Carved by fzgx. Replace this file's body with the matching C.
+extern u8 lbl_1_bss_3820C;
+
+u8 fn_1_40ED4(void) {
+    return lbl_1_bss_3820C;
+}
