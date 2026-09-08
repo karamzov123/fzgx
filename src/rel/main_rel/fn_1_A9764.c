fd3fe59420c69e08f32b7fcb73ec1124ec2beb4a
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:26:18 -0400

    match: main_rel/fn_1_A9764 (Matched indirect callback dispatch through lbl_1_bss_7167C.)

--- src/rel/main_rel/fn_1_A9764.c
+++ src/rel/main_rel/fn_1_A9764.c
@@ -1,4 +1,7 @@
 #include "types.h"
 
-// fn_1_A9764: main_rel .text:0x000A9764 size 0x2C
-// Carved by fzgx. Replace this file's body with the matching C.
+extern void (*lbl_1_bss_7167C)(void);
+
+void fn_1_A9764(void) {
+    lbl_1_bss_7167C();
+}
