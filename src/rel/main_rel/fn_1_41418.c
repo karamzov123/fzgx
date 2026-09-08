44c435e81c456389df26cd81f4956dbba6aeeebd
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:40:16 -0400

    match: main_rel/fn_1_41418 (Matched fn_1_41418 at 100.0% after 4 checks.)

--- src/rel/main_rel/fn_1_41418.c
+++ src/rel/main_rel/fn_1_41418.c
@@ -1,4 +1,29 @@
 #include "types.h"
 
-// fn_1_41418: main_rel .text:0x00041418 size 0x70
-// Carved by fzgx. Replace this file's body with the matching C.
+typedef struct Fn41418Data {
+    u32 count;
+    char *strings;
+} Fn41418Data;
+
+extern u32 strlen(const char *str);
+
+static inline u32 add_lengths(u32 length, u32 address) {
+    return length + address;
+}
+
+char *fn_1_41418(Fn41418Data *data, u32 index) {
+    u32 count = data->count & 0x7fffffff;
+    u32 str = (u32)data->strings;
+
+    if (index >= count) {
+        return 0;
+    }
+
+    while (index != 0) {
+        str = add_lengths(strlen((char *)str), str);
+        index -= 1;
+        str += 1;
+    }
+
+    return (char *)str;
+}
