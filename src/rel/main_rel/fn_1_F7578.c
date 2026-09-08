91422a0586b8723fa6cdd2a5acb00e9da768255d
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:39:19 -0400

    match: main_rel/fn_1_F7578 (Matched fn_1_F7578 at 100.0% with stack-local initialization and two buffer clears.)

--- src/rel/main_rel/fn_1_F7578.c
+++ src/rel/main_rel/fn_1_F7578.c
@@ -1,4 +1,18 @@
 #include "types.h"
 
-// fn_1_F7578: main_rel .text:0x000F7578 size 0x60
-// Carved by fzgx. Replace this file's body with the matching C.
+extern u8 lbl_1_bss_7F02C[16];
+extern u16 lbl_1_bss_7F03C[8];
+extern void fn_8001D32C(u32 *);
+extern void fn_80008BEC(void *, int, u32);
+
+void fn_1_F7578(void) {
+    u32 values[4];
+
+    values[0] = 2;
+    values[1] = 2;
+    values[2] = 2;
+    values[3] = 2;
+    fn_8001D32C(values);
+    fn_80008BEC(lbl_1_bss_7F02C, 0, 0x10);
+    fn_80008BEC(lbl_1_bss_7F03C, 0, 0x10);
+}
