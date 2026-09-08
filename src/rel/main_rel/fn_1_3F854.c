9305972838ca00e2e9aa9cdb2ae87ac85646cb31
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:35:23 -0400

    match: main_rel/fn_1_3F854 (Matched byte load from lbl_1_bss_3C30 offset 8.)

--- src/rel/main_rel/fn_1_3F854.c
+++ src/rel/main_rel/fn_1_3F854.c
@@ -1,4 +1,7 @@
 #include "types.h"
 
-// fn_1_3F854: main_rel .text:0x0003F854 size 0x10
-// Carved by fzgx. Replace this file's body with the matching C.
+extern u8 lbl_1_bss_3C30[5308];
+
+u8 fn_1_3F854(void) {
+    return lbl_1_bss_3C30[8];
+}
