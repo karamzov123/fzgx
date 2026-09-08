d58c166e175148df6bd85c1a2504a63a53ea23c0
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:34:56 -0400

    match: main_rel/fn_1_15B408 (Matched direct byte load from lbl_1_bss_8F878[0x3c].)

--- src/rel/main_rel/fn_1_15B408.c
+++ src/rel/main_rel/fn_1_15B408.c
@@ -1,4 +1,7 @@
 #include "types.h"
 
-// fn_1_15B408: main_rel .text:0x0015B408 size 0x10
-// Carved by fzgx. Replace this file's body with the matching C.
+extern u8 lbl_1_bss_8F878[88];
+
+u8 fn_1_15B408(void) {
+    return lbl_1_bss_8F878[0x3c];
+}
