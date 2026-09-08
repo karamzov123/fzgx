822dccdd0032fedef2337ef88693d50ff3907595
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:38:35 -0400

    match: main_rel/fn_1_547F8 (Matched linked-list callback traversal.)

--- src/rel/main_rel/fn_1_547F8.c
+++ src/rel/main_rel/fn_1_547F8.c
@@ -1,4 +1,15 @@
 #include "types.h"
 
-// fn_1_547F8: main_rel .text:0x000547F8 size 0x50
-// Carved by fzgx. Replace this file's body with the matching C.
+typedef struct fn_1_547F8_node {
+    struct fn_1_547F8_node *next;
+    void (*callback)(struct fn_1_547F8_node *);
+} fn_1_547F8_node;
+
+void fn_1_547F8(fn_1_547F8_node *node) {
+    while (node != 0) {
+        if (node->callback != 0) {
+            node->callback(node);
+        }
+        node = node->next;
+    }
+}
