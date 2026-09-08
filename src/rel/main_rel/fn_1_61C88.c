dcfe838610e6f12fc54909952e0a7b31f2fae4ad
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:36:24 -0400

    match: main_rel/fn_1_61C88 (Matched wrapper calling fn_1_61D08.)

--- src/rel/main_rel/fn_1_61C88.c
+++ src/rel/main_rel/fn_1_61C88.c
@@ -1,4 +1,7 @@
 #include "types.h"
 
-// fn_1_61C88: main_rel .text:0x00061C88 size 0x20
-// Carved by fzgx. Replace this file's body with the matching C.
+extern void fn_1_61D08(void);
+
+void fn_1_61C88(void) {
+    fn_1_61D08();
+}
