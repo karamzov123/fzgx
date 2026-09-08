ef968e70b9e6b94a79bd604284954c2fd7dcc183
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:35:02 -0400

    match: main_rel/fn_1_8DBA4 (Matched byte store to lbl_1_bss_6D820.)

--- src/rel/main_rel/fn_1_8DBA4.c
+++ src/rel/main_rel/fn_1_8DBA4.c
@@ -1,4 +1,7 @@
 #include "types.h"
 
-// fn_1_8DBA4: main_rel .text:0x0008DBA4 size 0xC
-// Carved by fzgx. Replace this file's body with the matching C.
+extern u8 lbl_1_bss_6D820;
+
+void fn_1_8DBA4(u8 value) {
+    lbl_1_bss_6D820 = value;
+}
