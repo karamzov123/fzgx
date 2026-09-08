bd70fea5a2fc4b0a81d9509a6975d2f1eede000c
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:40:51 -0400

    match: main_rel/fn_1_3F038 (Match fn_1_3F038 to retail assembly.)

--- src/rel/main_rel/fn_1_3F038.c
+++ src/rel/main_rel/fn_1_3F038.c
@@ -1,4 +1,30 @@
 #include "types.h"
 
-// fn_1_3F038: main_rel .text:0x0003F038 size 0x90
-// Carved by fzgx. Replace this file's body with the matching C.
+extern u8 lbl_1_bss_26C58;
+extern u8 lbl_1_bss_3C11;
+extern void fn_1_36330(void);
+extern void fn_1_36B10(void);
+extern void fn_1_3C98(void);
+extern u32 lbl_1_bss_3C30[1327];
+extern void fn_1_37AA8(void);
+extern void fn_1_37E98(void);
+
+int fn_1_3F038(void) {
+    if (lbl_1_bss_26C58 != 0) {
+        return 1;
+    }
+    if (lbl_1_bss_3C11 != 0) {
+        fn_1_36330();
+        lbl_1_bss_3C11 = 0;
+    }
+    fn_1_36B10();
+    fn_1_3C98();
+    if ((lbl_1_bss_3C30[0] & 0x00000004) == 0) {
+        fn_1_37AA8();
+    }
+    if (lbl_1_bss_26C58 != 0) {
+        fn_1_37E98();
+        return 1;
+    }
+    return 0;
+}
