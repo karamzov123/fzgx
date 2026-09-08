ac1b20d7923d9f24d7a998d37a65590f9dcb862b
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 22:37:33 -0400

    match: main_rel/fn_1_7899C (Zero the first byte of lbl_1_bss_6D14C array; simple byte store with immediate return.)

--- src/rel/main_rel/fn_1_7899C.c
+++ src/rel/main_rel/fn_1_7899C.c
@@ -1,4 +1,10 @@
 #include "types.h"
 
 // fn_1_7899C: main_rel .text:0x0007899C size 0x10
-// Carved by fzgx. Replace this file's body with the matching C.
+// Zero the first byte of lbl_1_bss_6D14C
+
+extern u8 lbl_1_bss_6D14C[92];
+
+void fn_1_7899C(void) {
+    lbl_1_bss_6D14C[0] = 0;
+}
