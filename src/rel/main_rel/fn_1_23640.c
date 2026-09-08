a578577cf8d80e9ba2d19a59e2febc46ab0e7be3
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:28:15 -0400

    match: main_rel/fn_1_23640 (Matched fn_1_23640 at 100.0% under GC/1.3.2.)

--- src/rel/main_rel/fn_1_23640.c
+++ src/rel/main_rel/fn_1_23640.c
@@ -1,4 +1,42 @@
 #include "types.h"
 
-// fn_1_23640: main_rel .text:0x00023640 size 0x8C
-// Carved by fzgx. Replace this file's body with the matching C.
+extern void *memset(void *, int, unsigned int);
+
+typedef struct Fn_1_23640 {
+    unsigned char pad00[0x80];
+    int field80;
+    unsigned char pad84[0x0c];
+    int *field90;
+    int *field94;
+    unsigned char pad98[0x04];
+    int field9c;
+    unsigned char padA0[0x18];
+    int fieldb8;
+    unsigned char padbc[0x0c];
+    int fieldc8;
+    unsigned char padcc[0x0c];
+    int fieldd8;
+    unsigned char pade0[0x40];
+    int field11c;
+    unsigned char pad120[0x3c];
+    int field15c;
+    unsigned char tail160[0x9c];
+} Fn_1_23640;
+
+void fn_1_23640(Fn_1_23640 *self, int value) {
+    if (self != 0) {
+        if (value != 0) {
+            memset(self, 0, 0x1fc);
+            self->field11c = -1;
+            self->field15c = -1;
+        } else {
+            memset(self, 0, 0x117);
+        }
+        self->field80 = value;
+        self->field90 = &self->fieldb8;
+        self->field94 = &self->fieldc8;
+        self->field9c = -1;
+        self->fieldb8 = -1;
+        self->fieldd8 = -1;
+    }
+}
