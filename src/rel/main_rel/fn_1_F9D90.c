9df973b6c3ee29dc9e63a07d9cf146ab2889c8a5
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:36:01 -0400

    match: main_rel/fn_1_F9D90 (Match global BSS halfword clear at offset 0x68.)

--- src/rel/main_rel/fn_1_F9D90.c
+++ src/rel/main_rel/fn_1_F9D90.c
@@ -1,4 +1,7 @@
 #include "types.h"
 
-// fn_1_F9D90: main_rel .text:0x000F9D90 size 0x14
-// Carved by fzgx. Replace this file's body with the matching C.
+extern u16 lbl_1_bss_7F0C0[5336];
+
+void fn_1_F9D90(void) {
+    lbl_1_bss_7F0C0[0x34] = 0;
+}
