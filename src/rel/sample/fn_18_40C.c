1a5e3de2adbe8af9cdd4ea0e885e20917eeb2533
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 22:22:16 -0400

    match: sample/fn_18_40C (LZ decode wrapper)

---
+++ src/rel/sample/fn_18_40C.c
@@ -0,0 +1,19 @@
+#include "types.h"
+
+typedef struct {
+    void *src;
+    void *dst;
+    u32 flags;
+} LzDecodeArgs;
+
+extern u32 *fn_18_444(LzDecodeArgs *args);
+
+// Wraps the LZ decoder: pack (src, dst) into an argument block and return the
+// first word of the decoder's result block.
+u32 fn_18_40C(void *src, void *dst) {
+    LzDecodeArgs args;
+    args.src = src;
+    args.dst = dst;
+    args.flags = 0;
+    return *fn_18_444(&args);
+}
