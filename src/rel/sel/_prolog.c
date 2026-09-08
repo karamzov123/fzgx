f4a1a371ae74150405e78c847ae9183718922b5e
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-08 11:29:50 -0400

    tu-finish sel: 98 declarations hoisted, 8 typedefs isolated, 31 contested, 13 unresolved

--- src/rel/sel/_prolog.c
+++ src/rel/sel/_prolog.c
@@ -1,4 +1,11 @@
 #include "types.h"
+extern struct Fn10Data lbl_10_bss_0;
+extern int fn_1_152970(int, int, int, int, struct Fn10Data *, int, int);
+extern u32 lbl_10_bss_49384;
+extern u32 lbl_10_bss_49388;
+extern void fn_1_435C(u32);
+extern void fn_1_407C(void);
+extern void fn_1_3CC4(void);
 
 /* fzgx:begin fn_10_0 */
 struct Fn10Data {
@@ -6,9 +13,6 @@ struct Fn10Data {
     s16 value_18;
     s16 value_1a;
 };
-
-extern struct Fn10Data lbl_10_bss_0;
-extern int fn_1_152970(int, int, int, int, struct Fn10Data *, int, int);
 
 s16 fn_10_0(int first, int second, int third, int fourth) {
     return (s16)fn_1_152970(second, first, third, fourth,
@@ -18,13 +22,6 @@ s16 fn_10_0(int first, int second, int third, int fourth) {
 /* fzgx:end fn_10_0 */
 
 /* fzgx:begin fn_10_AB8 */
-extern u32 lbl_10_bss_49384;
-extern u32 lbl_10_bss_49388;
-
-extern void fn_1_435C(u32);
-extern void fn_1_407C(void);
-extern void fn_1_3CC4(void);
-
 void fn_10_AB8(void) {
     fn_1_435C(lbl_10_bss_49384);
     fn_1_407C();
--- src/rel/sel/name_entry.c
+++ src/rel/sel/name_entry.c
@@ -1,4 +1,26 @@
 #include "types.h"
+extern u32 lbl_10_bss_55CD8;
+extern u8 lbl_10_bss_55CDC;
+extern u8 lbl_10_data_6980[64];
+extern s16 lbl_1_bss_960;
+extern u32 lbl_801A6410;
+extern void fn_1_435C(u32 value);
+extern void fn_1_426C(u32 value);
+extern void fn_1_A8F78(void);
+extern void fn_1_48140(int value);
+extern void fn_1_412A0(int value);
+extern u8 lbl_10_bss_55CE1;
+extern u8 fn_1_B7C00(void);
+extern u8 fn_10_26434(void);
+extern void fn_10_26554(void);
+extern u8 lbl_10_bss_55CE0;
+extern void fn_1_14BD94(void *);
+extern void fn_10_26424(void);
+extern u16 lbl_1_bss_9F8[5];
+extern u8 lbl_1_bss_8B3A0[0x9f];
+extern void fn_1_A2D84(void *);
+extern void fn_1_14BCBC(void *, u8);
+extern u8 lbl_10_bss_55CE2;
 
 /* fzgx:begin fn_10_25BC8 */
 typedef struct NameEntryState {
@@ -10,17 +32,6 @@ typedef struct NameEntryState {
     u32 unk1c;
 } NameEntryState;
 
-extern u32 lbl_10_bss_55CD8;
-extern u8 lbl_10_bss_55CDC;
-extern u8 lbl_10_data_6980[64];
-extern s16 lbl_1_bss_960;
-extern u32 lbl_801A6410;
-
-extern void fn_1_435C(u32 value);
-extern void fn_1_426C(u32 value);
-extern void fn_1_A8F78(void);
-extern void fn_1_48140(int value);
-extern void fn_1_412A0(int value);
 extern void fn_1_46B4(u32 arg0, NameEntryState *arg1, u8 *arg2, int arg3);
 
 void fn_10_25BC8(void) {
@@ -47,18 +58,6 @@ void fn_10_25BC8(void) {
 /* fzgx:end fn_10_25BC8 */
 
 /* fzgx:begin fn_10_26000 */
-extern u8 lbl_10_bss_55CE1;
-extern u8 fn_1_B7C00(void);
-extern u8 fn_10_26434(void);
-extern void fn_10_26554(void);
-extern u8 lbl_10_bss_55CE0;
-extern void fn_1_14BD94(void *);
-extern void fn_10_26424(void);
-extern u16 lbl_1_bss_9F8[5];
-extern u8 lbl_1_bss_8B3A0[0x9f];
-extern void fn_1_A2D84(void *);
-extern void fn_1_14BCBC(void *, u8);
-
 u8 fn_10_26000(void) {
     u8 result;
     u16 flags;
@@ -92,8 +91,6 @@ u8 fn_10_26000(void) {
 /* fzgx:end fn_10_26000 */
 
 /* fzgx:begin fn_10_26424 */
-extern u8 lbl_10_bss_55CE2;
-
 void fn_10_26424(void) {
     lbl_10_bss_55CE2 = 1;
 }
