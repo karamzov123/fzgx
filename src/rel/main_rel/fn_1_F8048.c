dc3e2184d6da151d959b2884ab01a6b36f772b3a
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:27:00 -0400

    match: main_rel/fn_1_F8048 (Match loop over s16 indices 0..74; invoke fn_1_F8214 when fn_1_14D6D8 returns bit 30 set.)

--- src/rel/main_rel/fn_1_F8048.c
+++ src/rel/main_rel/fn_1_F8048.c
@@ -1,4 +1,14 @@
 #include "types.h"
 
-// fn_1_F8048: main_rel .text:0x000F8048 size 0x54
-// Carved by fzgx. Replace this file's body with the matching C.
+extern int fn_1_14D6D8(s16 value);
+extern void fn_1_F8214(s16 value);
+
+void fn_1_F8048(void) {
+    s16 i;
+
+    for (i = 0; i < 75; i++) {
+        if (fn_1_14D6D8(i) & 0x40000000) {
+            fn_1_F8214(i);
+        }
+    }
+}
