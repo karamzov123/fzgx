46a171ef6566ef7d21f8a8688d37e768a225e882
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:39:22 -0400

    match: main_rel/fn_1_21CA0 (Matched fn_1_21CA0 at 100.0% under GC/1.3.2.)

--- src/rel/main_rel/fn_1_21CA0.c
+++ src/rel/main_rel/fn_1_21CA0.c
@@ -1,4 +1,28 @@
 #include "types.h"
 
-// fn_1_21CA0: main_rel .text:0x00021CA0 size 0x5C
-// Carved by fzgx. Replace this file's body with the matching C.
+extern f32 lbl_1_rodata_6D0;
+extern void fn_1_21950(void *, f32, f32);
+
+typedef struct Fn_1_21CA0 {
+    u32 flags;
+    unsigned char pad04[0x38];
+    f32 field3c;
+    f32 field40;
+} Fn_1_21CA0;
+
+void fn_1_21CA0(Fn_1_21CA0 *self, void *arg) {
+    f32 value2;
+    f32 value1;
+
+    if ((self->flags & 0x800) != 0) {
+        value2 = self->field3c;
+    } else {
+        value2 = lbl_1_rodata_6D0;
+    }
+    if ((self->flags & 0x400) != 0) {
+        value1 = self->field40;
+    } else {
+        value1 = lbl_1_rodata_6D0;
+    }
+    fn_1_21950(arg, value1, value2);
+}
