85d97eb1d5a6e537abeb3a4e67c7bfd9ce33c91f
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:25:56 -0400

    match: main_rel/fn_1_154F74 (match fn_1_154F74)

--- src/rel/main_rel/fn_1_154F74.c
+++ src/rel/main_rel/fn_1_154F74.c
@@ -1,4 +1,8 @@
 #include "types.h"
 
-// fn_1_154F74: main_rel .text:0x00154F74 size 0x14
-// Carved by fzgx. Replace this file's body with the matching C.
+extern u32 lbl_1_bss_8EDF0[76];
+
+void fn_1_154F74(u32 arg0, u32 arg1) {
+    lbl_1_bss_8EDF0[8] = arg0;
+    lbl_1_bss_8EDF0[9] = arg1;
+}
