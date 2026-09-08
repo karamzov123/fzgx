67ddeffce299b64af044067e83aa3619e8fa3e25
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:38:13 -0400

    match: main_rel/fn_1_9FF9C (Matched fn_1_9FF9C at 100.0% on GC/1.3.2.)

--- src/rel/main_rel/fn_1_9FF9C.c
+++ src/rel/main_rel/fn_1_9FF9C.c
@@ -1,4 +1,13 @@
 #include "types.h"
 
-// fn_1_9FF9C: main_rel .text:0x0009FF9C size 0x3C
-// Carved by fzgx. Replace this file's body with the matching C.
+extern u8 lbl_1_bss_6EAA8[8];
+extern u8 lbl_1_bss_6EAB0;
+
+extern void fn_8001EA30(u8 *arg0, int arg1);
+extern void fn_80020530(void);
+
+void fn_1_9FF9C(void) {
+    fn_8001EA30(lbl_1_bss_6EAA8, 2);
+    fn_80020530();
+    lbl_1_bss_6EAB0 = 0;
+}
