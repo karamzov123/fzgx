719989465780a10ef5c2da62168ba910030f1cf8
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:31:58 -0400

    match: main_rel/fn_1_D76F0 (Match fn_1_D76F0 with indexed store followed by pointer advance.)

--- src/rel/main_rel/fn_1_D76F0.c
+++ src/rel/main_rel/fn_1_D76F0.c
@@ -1,4 +1,18 @@
 #include "types.h"
 
-// fn_1_D76F0: main_rel .text:0x000D76F0 size 0x34
-// Carved by fzgx. Replace this file's body with the matching C.
+typedef struct {
+    f32 x;
+    f32 y;
+    f32 z;
+} Vec3;
+
+extern u32 lbl_1_data_2A7E0[62];
+
+void fn_1_D76F0(const Vec3 *src, s16 index) {
+    f32 *dst = (f32 *)lbl_1_data_2A7E0[15];
+    dst[index * 3] = src->x;
+    dst += index * 3;
+    dst[1] = src->y;
+    dst[2] = src->z;
+}
+
