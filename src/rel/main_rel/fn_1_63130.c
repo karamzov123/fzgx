9fa36780fe0571e8331b9f293aac9b41bb3a0e19
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:35:26 -0400

    match: main_rel/fn_1_63130 (Matched zeroing the s16 field at offset 0xAE.)

--- src/rel/main_rel/fn_1_63130.c
+++ src/rel/main_rel/fn_1_63130.c
@@ -1,4 +1,10 @@
 #include "types.h"
 
-// fn_1_63130: main_rel .text:0x00063130 size 0xC
-// Carved by fzgx. Replace this file's body with the matching C.
+struct fn_1_63130_obj {
+    u8 unk_00[0xAE];
+    s16 unk_AE;
+};
+
+void fn_1_63130(struct fn_1_63130_obj *obj) {
+    obj->unk_AE = 0;
+}
