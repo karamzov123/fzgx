806c0c7b2251c2a713a0a8eb829ba951e7740975
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 22:26:59 -0400

    match: main_rel/fn_1_D960 (Returns address of u16 array lbl_1_bss_1018)

---
+++ src/rel/main_rel/fn_1_D960.c
@@ -0,0 +1,10 @@
+#include "types.h"
+
+// fn_1_D960: main_rel .text:0x0000D960 size 0xC
+// Returns address of lbl_1_bss_1018
+
+extern u16 lbl_1_bss_1018[18];
+
+u16* fn_1_D960(void) {
+    return lbl_1_bss_1018;
+}
