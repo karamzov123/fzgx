22243329edbcc03f49d5320485cb31582545b6e0
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:39:40 -0400

    match: main_rel/fn_1_80058 (Match fn_1_80058)

--- src/rel/main_rel/fn_1_80058.c
+++ src/rel/main_rel/fn_1_80058.c
@@ -1,4 +1,14 @@
 #include "types.h"
 
-// fn_1_80058: main_rel .text:0x00080058 size 0x6C
-// Carved by fzgx. Replace this file's body with the matching C.
+extern void fn_80008BEC(void *dst, s32 value, u32 size);
+extern void fn_80008BA8(void *dst, const void *src, u32 size);
+
+void fn_1_80058(s8 arg0, u8 *arg1) {
+    u8 data[0x20];
+
+    fn_80008BEC(data, 0, 0x20);
+    *(u32 *)(data + 4) = 0xF;
+    data[8] = 1;
+    fn_80008BA8(arg1, data, 0x20);
+    *(u16 *)(arg1 + 2) = arg0;
+}
