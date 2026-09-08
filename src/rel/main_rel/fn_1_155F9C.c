3fb0094cf63b47f41c88afe57531cb398169c75d
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:25:52 -0400

    match: main_rel/fn_1_155F9C (Match fn_1_155F9C global BSS store.)

--- src/rel/main_rel/fn_1_155F9C.c
+++ src/rel/main_rel/fn_1_155F9C.c
@@ -1,4 +1,7 @@
 #include "types.h"
 
-// fn_1_155F9C: main_rel .text:0x00155F9C size 0xC
-// Carved by fzgx. Replace this file's body with the matching C.
+extern u32 lbl_1_bss_8F3E0;
+
+void fn_1_155F9C(u32 value) {
+    lbl_1_bss_8F3E0 = value;
+}
