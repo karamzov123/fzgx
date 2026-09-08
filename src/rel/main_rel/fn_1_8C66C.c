e1c574e16a0e3f4325d291ba4e61c395405d996c
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:37:52 -0400

    match: main_rel/fn_1_8C66C (Match fn_1_8C66C)

--- src/rel/main_rel/fn_1_8C66C.c
+++ src/rel/main_rel/fn_1_8C66C.c
@@ -1,4 +1,20 @@
 #include "types.h"
 
-// fn_1_8C66C: main_rel .text:0x0008C66C size 0x34
-// Carved by fzgx. Replace this file's body with the matching C.
+extern u32 lbl_1_bss_6D838;
+
+u32 fn_1_8C66C(u32 index) {
+    u32 base;
+    u32 addr;
+    u32 ptr;
+
+    base = lbl_1_bss_6D838;
+    if (base != 0) {
+        addr = base + index * 0x620;
+    } else {
+        addr = 0;
+    }
+
+    ptr = *(u32 *)(addr + 0x49c);
+    ptr = *(u32 *)(ptr + 0x90);
+    return *(u32 *)ptr;
+}
