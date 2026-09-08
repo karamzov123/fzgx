268d56da722d0905c243e9b760029f1d1bc0f3a4
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:37:26 -0400

    match: main_rel/fn_1_14BD44 (Matched argument reorder wrapper and zero third argument.)

--- src/rel/main_rel/fn_1_14BD44.c
+++ src/rel/main_rel/fn_1_14BD44.c
@@ -1,4 +1,8 @@
 #include "types.h"
 
-// fn_1_14BD44: main_rel .text:0x0014BD44 size 0x30
-// Carved by fzgx. Replace this file's body with the matching C.
+extern void fn_1_14BFD8(void *, void *, int);
+
+void fn_1_14BD44(void *arg0, void *arg1) {
+    fn_1_14BFD8(arg1, arg0, 0);
+}
+
