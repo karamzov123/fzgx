1b1ba9fd325998da59a3d3aef95fa6ee1a828408
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-07 23:34:46 -0400

    match: main_rel/fn_1_45BE0 (Matched global zero-initializer function.)

--- src/rel/main_rel/fn_1_45BE0.c
+++ src/rel/main_rel/fn_1_45BE0.c
@@ -1,4 +1,7 @@
 #include "types.h"
 
-// fn_1_45BE0: main_rel .text:0x00045BE0 size 0x10
-// Carved by fzgx. Replace this file's body with the matching C.
+extern u32 lbl_1_bss_384C8;
+
+void fn_1_45BE0(void) {
+    lbl_1_bss_384C8 = 0;
+}
